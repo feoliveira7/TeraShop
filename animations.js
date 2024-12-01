10939a0f85ef07b506a7f0021c6639f065d8d649
Felipe <126893383+feoliveira7@users.noreply.github.com>
2024-12-01 17:09:16 -0300

    Add files via upload

---
+++ animations.js
@@ -0,0 +1,48 @@
+const botaoSlider1 = document.getElementById('buttonSlider1')
+const botaoSlider2 = document.getElementById('buttonSlider2')
+const botaoSlider3 = document.getElementById('buttonSlider3')
+
+function image1(){
+    botaoSlider1.style.width='55px'
+    botaoSlider1.style.opacity='100%'
+    botaoSlider1.style.boxShadow='0px 0px 10px rgba(216, 216, 216, 0.692)'
+
+    botaoSlider2.style.width='42px'
+    botaoSlider2.style.opacity='60%'
+    botaoSlider2.style.boxShadow='0px 0px 10px rgba(216, 216, 216, 0)'
+
+    botaoSlider3.style.width='42px'
+    botaoSlider3.style.opacity='60%'
+    botaoSlider3.style.boxShadow='0px 0px 10px rgba(216, 216, 216, 0)'
+
+}
+
+function image2(){
+    botaoSlider2.style.width='55px'
+    botaoSlider2.style.opacity='100%'
+    botaoSlider2.style.boxShadow='0px 0px 10px rgba(216, 216, 216, 0.692)'
+
+    botaoSlider1.style.width='42px'
+    botaoSlider1.style.opacity='60%'
+    botaoSlider1.style.boxShadow='0px 0px 10px rgba(216, 216, 216, 0)'
+
+    botaoSlider3.style.width='42px'
+    botaoSlider3.style.opacity='60%'
+    botaoSlider3.style.boxShadow='0px 0px 10px rgba(216, 216, 216, 0)'
+
+}
+
+function image3(){
+    botaoSlider3.style.width='55px'
+    botaoSlider3.style.opacity='100%'
+    botaoSlider3.style.boxShadow='0px 0px 10px rgba(216, 216, 216, 0.692)'
+
+    botaoSlider2.style.width='42px'
+    botaoSlider2.style.opacity='60%'
+    botaoSlider2.style.boxShadow='0px 0px 10px rgba(216, 216, 216, 0)'
+
+    botaoSlider1.style.width='42px'
+    botaoSlider1.style.opacity='60%'
+    botaoSlider1.style.boxShadow='0px 0px 10px rgba(216, 216, 216, 0)'
+
+}
---
+++ produtos.js
@@ -0,0 +1,69 @@
+
+
+fetch('produtos.json').then(resposta => resposta.json()).then(corpo =>{
+
+    document.getElementById('imageProduto1').src = corpo.imageProduto1;
+    document.getElementById('nomeProduto1').innerHTML = corpo.nomeProduto1;
+    document.getElementById('ValorProduto1').innerHTML = corpo.valorProduto1;
+
+    document.getElementById('imageProduto2').src = corpo.imageProduto2;
+    document.getElementById('nomeProduto2').innerHTML = corpo.nomeProduto2;
+    document.getElementById('ValorProduto2').innerHTML = corpo.valorProduto2;
+
+    document.getElementById('imageProduto3').src = corpo.imageProduto3;
+    document.getElementById('nomeProduto3').innerHTML = corpo.nomeProduto3;
+    document.getElementById('ValorProduto3').innerHTML = corpo.valorProduto3;
+
+    document.getElementById('imageProduto4').src = corpo.imageProduto4;
+    document.getElementById('nomeProduto4').innerHTML = corpo.nomeProduto4;
+    document.getElementById('ValorProduto4').innerHTML = corpo.valorProduto4;
+
+    document.getElementById('imageProduto5').src = corpo.imageProduto5;
+    document.getElementById('nomeProduto5').innerHTML = corpo.nomeProduto5;
+    document.getElementById('ValorProduto5').innerHTML = corpo.valorProduto5;
+
+    document.getElementById('imageProduto6').src = corpo.imageProduto6;
+    document.getElementById('nomeProduto6').innerHTML = corpo.nomeProduto6;
+    document.getElementById('ValorProduto6').innerHTML = corpo.valorProduto6;
+
+    document.getElementById('imageProduto7').src = corpo.imageProduto7;
+    document.getElementById('nomeProduto7').innerHTML = corpo.nomeProduto7;
+    document.getElementById('ValorProduto7').innerHTML = corpo.valorProduto7;
+
+    document.getElementById('imageProduto8').src = corpo.imageProduto8;
+    document.getElementById('nomeProduto8').innerHTML = corpo.nomeProduto8;
+    document.getElementById('ValorProduto8').innerHTML = corpo.valorProduto8;
+
+    document.getElementById('imageProduto9').src = corpo.imageProduto9;
+    document.getElementById('nomeProduto9').innerHTML = corpo.nomeProduto9;
+    document.getElementById('ValorProduto9').innerHTML = corpo.valorProduto9;
+
+    document.getElementById('imageProduto10').src = corpo.imageProduto10;
+    document.getElementById('nomeProduto10').innerHTML = corpo.nomeProduto10;
+    document.getElementById('ValorProduto10').innerHTML = corpo.valorProduto10;
+
+    document.getElementById('imageProduto11').src = corpo.imageProduto11;
+    document.getElementById('nomeProduto11').innerHTML = corpo.nomeProduto11;
+    document.getElementById('ValorProduto11').innerHTML = corpo.valorProduto11;
+
+    document.getElementById('imageProduto12').src = corpo.imageProduto12;
+    document.getElementById('nomeProduto12').innerHTML = corpo.nomeProduto12;
+    document.getElementById('ValorProduto12').innerHTML = corpo.valorProduto12;
+
+    document.getElementById('imageProduto13').src = corpo.imageProduto13;
+    document.getElementById('nomeProduto13').innerHTML = corpo.nomeProduto13;
+    document.getElementById('ValorProduto13').innerHTML = corpo.valorProduto13;
+
+    document.getElementById('imageProduto14').src = corpo.imageProduto14;
+    document.getElementById('nomeProduto14').innerHTML = corpo.nomeProduto14;
+    document.getElementById('ValorProduto14').innerHTML = corpo.valorProduto14;
+
+    document.getElementById('imageProduto15').src = corpo.imageProduto15;
+    document.getElementById('nomeProduto15').innerHTML = corpo.nomeProduto15;
+    document.getElementById('ValorProduto15').innerHTML = corpo.valorProduto15;
+
+    document.getElementById('imageProduto16').src = corpo.imageProduto16;
+    document.getElementById('nomeProduto16').innerHTML = corpo.nomeProduto16;
+    document.getElementById('ValorProduto16').innerHTML = corpo.valorProduto16;
+
+})
